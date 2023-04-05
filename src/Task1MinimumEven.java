b6845646c9d02d968d3c46cf2942a5e1c94e4f1f
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-05 08:01:29 +0200

    add draft of logic

--- src/Task1MinimumEven.java
+++ src/Task1MinimumEven.java
@@ -4,6 +4,41 @@
 //
 //Оценить временную и пространственную сложность алгоритма.
 
-public class Task1MinimumEven {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Task1MinimumEven {
+  public static void main(String[] args) throws IOException {
+    BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+
+    System.out.println("Enter the quantity of numbers in sequence");
+    int numbers = Integer.parseInt(read.readLine());
+
+    System.out.println("Enter the numbers (each number from new line):");
+
+    int result = Integer.MAX_VALUE;
+    boolean findNewResult = false;
+    int incomeNumber = -1;
+
+    for(int i = 0; i < numbers; ++i) {
+      incomeNumber = Integer.parseInt(read.readLine());
+
+      if (incomeNumber < 0) {
+        System.err.println("Incorrect input number" + incomeNumber);
+        break;
+      }
+
+      if ((incomeNumber % 2 == 0) && (incomeNumber < result)) {
+        result = incomeNumber;
+        findNewResult = true;
+      }
+    }
+
+    if (findNewResult) {
+      System.out.println(incomeNumber);
+    } else {
+      System.out.println("-1");
+    }
+  }
 }
